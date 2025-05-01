679a4863f16e300513e2ef8f57459963bb547855
ipash185 <127605678+ipash185@users.noreply.github.com>
2025-05-01 11:29:45 +0530

    hosting changes

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],    # frontend URL
+    allow_origins=["http://localhost:5173", "https://cruiseaide.netlify.app/"],    # frontend URL
     allow_credentials=True,                     # allow cookies
     allow_methods=["*"],
     allow_headers=["*"],
